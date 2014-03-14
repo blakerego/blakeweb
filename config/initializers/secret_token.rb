0d01153cd48f52a15f33ade204397eebd2df3b8e
Blake Rego <blake.rego@gmail.com>
2014-03-13 21:15:35 -0400

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Detroit::Application.config.secret_key_base = 'c70a7398491305077e8fb507933ebab557bd3fbc5cb30811b4af9fcffdf6bf4817d0717d50d7c0402367ce621947d22b5ef5ecf797b61b6aca16d771a72d1e3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Detroit::Application.config.session_store :cookie_store, key: '_detroit_session'
