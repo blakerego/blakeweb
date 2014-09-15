21401b048938aac2e3978e6add4289d52b883aac
Blake Rego <blake.rego@gmail.com>
2014-09-15 19:55:24 -0400

    Moving javascript from actual template into controller.

--- app/assets/javascripts/index-ctrl.js
+++ app/assets/javascripts/index-ctrl.js
@@ -6,4 +6,33 @@ angular.module('blakeHome', ['balloonModel'])
 
   $scope.balloonModel = balloonModel;
 
+  var menuElement = $('.focusOnHover').first();
+  if (menuElement.offset().top + menuElement.height() < document.documentElement.clientHeight) {
+    $('body').css('overflow', 'hidden');
+  }
+
+  var demo, playing, renderer, container;
+
+  function init() {
+    playing = true;
+    renderer = 'WebGLRenderer';
+    container = $('#container');
+    if (demo) {
+      demo.destroy();
+      demo = null;
+    }
+    demo = new BlakeDemo();
+    demo.init(container.get(0), new self[ renderer ]());
+    update();
+  }
+
+  function update() {
+    requestAnimationFrame(update);
+    if (playing && demo) {
+      demo.step();
+    }
+  }
+
+  init();
+
 });
