f919de05db1ff19482bf169e79bcfa274c10d225
Blake Rego <blake.rego@gmail.com>
2014-09-17 21:00:54 -0400

    Some cleanup

--- app/assets/javascripts/index-ctrl.js
+++ app/assets/javascripts/index-ctrl.js
@@ -1,5 +1,4 @@
 angular.module('blakeHome', [
-  'balloonModel',
   'physContainer',
   'WebGLRenderer',
   'BlakeDemo',
@@ -7,39 +6,13 @@ angular.module('blakeHome', [
   ])
 .controller('indexCtrl', function (
   $scope,
-  balloonModel,
   BlakeDemo,
   WebGLRenderer
   ) {
-
-  // $scope.balloonModel = balloonModel;
 
   var menuElement = $('.focusOnHover').first();
   if (menuElement.offset().top + menuElement.height() < document.documentElement.clientHeight) {
     $('body').css('overflow', 'hidden');
   }
-
-  // var demo, playing, renderer, container;
-
-  // function init() {
-  //   playing = true;
-  //   container = $('#container');
-  //   if (demo) {
-  //     demo.destroy();
-  //     demo = null;
-  //   }
-  //   demo = new BlakeDemo();
-  //   demo.init(container.get(0), new WebGLRenderer());
-  //   update();
-  // }
-
-  // function update() {
-  //   requestAnimationFrame(update);
-  //   if (playing && demo) {
-  //     demo.step();
-  //   }
-  // }
-
-  // init();
 
 });
